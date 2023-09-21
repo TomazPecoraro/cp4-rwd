3dc0e4d93d6ff967286a71aceecaaafa3c6e4669
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-09-21 20:37:28 -0300

    Navigate, delete, voltar

--- src/routes/VisualizarAparelho/index.jsx
+++ src/routes/VisualizarAparelho/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import aparelhosData from '../../data/aparelhosData.jsx';
 import styles from './index.module.css';
 
 function VisualizarAparelho() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const aparelho = aparelhosData.find((item) => item.id === parseInt(id));
+  const handleDelete = () => {
+    const index = aparelhosData.findIndex((item) => item.id ===
+    aparelho.id);
+    if (index != -1) {
+      aparelhosData.splice(index, 1);
+      navigate('/aparelhos');
+    }
+  };
 
   if (!aparelho) {
     return <p>Aparelho não encontrado.</p>;
@@ -13,7 +22,7 @@ function VisualizarAparelho() {
 
   return (
     <div className={styles.container}>
-      <h2>{aparelho.nome}</h2>
+      <h2>Detalhes de {aparelho.nome}</h2>
       <img
         src={aparelho.imagem}
         alt={aparelho.nome}
@@ -21,7 +30,8 @@ function VisualizarAparelho() {
       />
       <p className={styles.descricaoExtensa}>{aparelho.descricaoExtensa}</p>
       <p className={styles.preco}>Preço: ${aparelho.preco}</p>
-      <a href="/aparelhos" className={styles.goBackLink}>Voltar</a>
+      <button onClick={handleDelete} className={styles.goBackLink}>Excluir Aparelho</button>
+      <button onClick={() => navigate('/aparelhos')} className={styles.goBackLink}>Voltar lista de Aparelhos</button>
     </div>
   );
 }
